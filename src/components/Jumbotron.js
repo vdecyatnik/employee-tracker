9d320ce8233daf3fcc1b4978889bfbeb61416b35
Val <vdecyatnik@gmail.com>
2021-02-23 22:14:53 -0800

    initial commit

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+
+function Jumbotron() {
+    return (
+
+
+<nav className="navbar navbar-light bg-light">
+  <div className="container-fluid">
+    <span className="navbar-brand mb-0 h1">Navbar</span>
+  </div>
+</nav>
+
+);
+
+}
+
+export default Jumbotron;
